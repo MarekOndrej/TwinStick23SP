aab715251e7d501b5b41411b0f41a97a18316991
Maara <fmava006@edu.xamk.fi>
2026-01-15 14:20:14 +0200

    enemy spawner coroutines added

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
@@ -6,12 +7,41 @@ public class EnemySpawner : MonoBehaviour
 
     [SerializeField] GameObject chaseTarget;
 
+    [SerializeField] float waitSeconds = 4f;
+    [SerializeField] float betweenEnemies = 5f;
+
     private void Start()
     {
         chaseTarget = GameObject.Find("ChaseTarget");
+        SpawnEnemy();
+
+        StartCoroutine(enemySpawnDelay(waitSeconds, betweenEnemies));
+    }
+
+    private void SpawnEnemy()
+    {
         Enemy newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
         newEnemy.SetChaseTarget(chaseTarget);
     }
 
+    IEnumerator enemySpawnDelay(float startDelay, float delayBetweenEnemies)
+    {
+        yield return new WaitForSeconds(startDelay);
 
+
+        SpawnEnemy();
+
+        yield return new WaitForSeconds(delayBetweenEnemies);
+
+        SpawnEnemy();
+
+    }
+
+
+    //IEnumerator Pause(float delay)
+    //{
+    //    Debug.Log("starting pause");
+    //    yield return new WaitForSeconds(delay);
+    //    Debug.Log("pause is over");
+    //}
 }
